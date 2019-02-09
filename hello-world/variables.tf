9da3c63ffb2ab189ac866197e922d02e49f3d81a
Giulio Vian <giuliovdev@hotmail.com>
2019-02-09 14:27:29 +0000

    hello world sample

---
+++ hello-world/variables.tf
@@ -0,0 +1,18 @@
+### VARIABLES
+
+variable "azurerm_subscription_id" {}
+variable "azurerm_client_id" {}
+variable "azurerm_client_secret" {}
+variable "azurerm_tenant_id" {}
+
+variable "env_name" {
+  description = "Name of Environment"
+  default     = "tf-demo-ldna"
+}
+
+variable "resource_group_location" {
+  default = "westeurope"
+}
+
+# EOF #
+
