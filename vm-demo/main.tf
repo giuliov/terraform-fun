cb87748c783e05a76743a5417ba204104067c5af
Giulio Vian <giuliovdev@hotmail.com>
2019-03-14 11:40:16 +0000

    removed the variables from the VM demo in favour of environment variables

--- vm-demo/main.tf
+++ vm-demo/main.tf
@@ -13,10 +13,6 @@ terraform {
 
 provider "azurerm" {
   version         = "~> 1.0"
-  subscription_id = "${var.azurerm_subscription_id}"
-  client_id       = "${var.azurerm_client_id}"
-  client_secret   = "${var.azurerm_client_secret}"
-  tenant_id       = "${var.azurerm_tenant_id}"
 }
 
 provider "external" {
