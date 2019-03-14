93ba870435698908168308eed50f4375e854d054
Giulio Vian <giuliovdev@hotmail.com>
2019-03-14 11:39:11 +0000

    some refactor and renaming in Hello World example

--- hello-world/main.tf
+++ hello-world/main.tf
@@ -1,18 +1,6 @@
 ### MAIN
 
-terraform {
-  required_version = "~> 0.11.1"
-}
-
-provider "azurerm" {
-  version         = "~> 1.0"
-  subscription_id = "${var.azurerm_subscription_id}"
-  client_id       = "${var.azurerm_client_id}"
-  client_secret   = "${var.azurerm_client_secret}"
-  tenant_id       = "${var.azurerm_tenant_id}"
-}
-
-resource "azurerm_resource_group" "vm_demo" {
+resource "azurerm_resource_group" "hello_demo" {
   name     = "${var.env_name}"
   location = "${var.resource_group_location}"
 
---
+++ hello-world/outputs.tf
@@ -0,0 +1,6 @@
+output "rg_id" {
+  value = "azurerm_resource_group.hello_demo.id"
+}
+
+# EOF
+
