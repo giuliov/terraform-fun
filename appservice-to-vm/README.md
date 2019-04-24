6ca60a6d8d7f8fb24389feff9cfcfe2c225161e1
Giulio Vian <giuliovdev@hotmail.com>
2019-04-24 07:58:50 +0100

    This works after a few manual steps

--- appservice-to-vm/README.md
+++ appservice-to-vm/README.md
@@ -2,3 +2,8 @@ This demo shows how to setup an App Service that connects to an existing resourc
 SQL Server database running inside a VM.
 
 See [Integrate your app with an Azure Virtual Network](https://docs.microsoft.com/en-us/azure/app-service/web-sites-integrate-with-vnet) for more details.
+
+Manual steps:
+1. The `custom_data` does not work, so complete SQL setup and start it
+2. **VNet Integration** Add VNet from App Service/Networking
+3. **VNet Integration** in the App Service Plan, find _Sync Network_ and run it
--- appservice-to-vm/security-group.tf
+++ appservice-to-vm/security-group.tf
@@ -19,21 +19,35 @@ resource "azurerm_network_security_rule" "appsvcint_demo_ssh_in" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = "${data.external.client_ip.result["ip"]}"
+  source_address_prefix       = "${data.external.client_ip.result["ip"]}/32"
   destination_address_prefix  = "${azurerm_subnet.appsvcint_demo_db.address_prefix}"
   resource_group_name         = "${azurerm_resource_group.appsvcint_demo.name}"
   network_security_group_name = "${azurerm_network_security_group.appsvcint_demo.name}"
 }
 
-resource "azurerm_network_security_rule" "appsvcint_demo_tsql_in" {
-  name                        = "${var.env_name}-tsql-in"
+resource "azurerm_network_security_rule" "appsvcint_demo_tsql_in1" {
+  name                        = "${var.env_name}-tsql-in1"
   priority                    = "210"
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "1433"
-  source_address_prefixes     = ["VirtualNetwork", "${data.external.client_ip.result["ip"]}"]
+  source_address_prefix       = "VirtualNetwork"
+  destination_address_prefix  = "${azurerm_subnet.appsvcint_demo_db.address_prefix}"
+  resource_group_name         = "${azurerm_resource_group.appsvcint_demo.name}"
+  network_security_group_name = "${azurerm_network_security_group.appsvcint_demo.name}"
+}
+
+resource "azurerm_network_security_rule" "appsvcint_demo_tsql_in2" {
+  name                        = "${var.env_name}-tsql-in2"
+  priority                    = "211"
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "1433"
+  source_address_prefix       = "${data.external.client_ip.result["ip"]}/32"
   destination_address_prefix  = "${azurerm_subnet.appsvcint_demo_db.address_prefix}"
   resource_group_name         = "${azurerm_resource_group.appsvcint_demo.name}"
   network_security_group_name = "${azurerm_network_security_group.appsvcint_demo.name}"
