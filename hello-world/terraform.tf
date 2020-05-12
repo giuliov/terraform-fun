6211b06c31fabe03ba14e6b6cab43efce1845e6b
Giulio Vian <giuliovdev@hotmail.com>
2020-05-12 20:55:22 +0100

    remove use of variables in hello-world sample

--- hello-world/terraform.tf
+++ hello-world/terraform.tf
@@ -4,10 +4,10 @@ terraform {
 
 provider "azurerm" {
   version         = "~> 1.0"
-  subscription_id = var.azurerm_subscription_id
-  client_id       = var.azurerm_client_id
-  client_secret   = var.azurerm_client_secret
-  tenant_id       = var.azurerm_tenant_id
+  # subscription_id = var.azurerm_subscription_id
+  # client_id       = var.azurerm_client_id
+  # client_secret   = var.azurerm_client_secret
+  # tenant_id       = var.azurerm_tenant_id
 }
 
 # EOF
--- hello-world/variables.tf
+++ hello-world/variables.tf
@@ -1,9 +1,9 @@
 ### VARIABLES
 
-variable "azurerm_subscription_id" {}
-variable "azurerm_client_id" {}
-variable "azurerm_client_secret" {}
-variable "azurerm_tenant_id" {}
+# variable "azurerm_subscription_id" {}
+# variable "azurerm_client_id" {}
+# variable "azurerm_client_secret" {}
+# variable "azurerm_tenant_id" {}
 
 variable "env_name" {
   description = "Name of Environment"
